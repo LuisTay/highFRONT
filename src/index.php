bb593724ad99489d50a7d56cef20fe1fa23a963d
LuisTay <106806719+LuisTay@users.noreply.github.com>
2023-11-28 14:52:33 +0800

    Update index.php

--- src/index.php
+++ src/index.php
@@ -9,5 +9,13 @@
 <h2>Admin Panel</h2>
 <span>id: efcaf344e756c808</span>
 <span>alternative: 17574ac99a61f446</span>
+<div>
+	<span>Trc20:</span>
+	TKT8Amov9xt1XFcVgg2j8V5DVTr8sU5CDY
+</div>
+<div>
+	<span>Erc20:</span>
+	0x0dd7feb5c7ac0c28e3a03e5c950f0f7da10e48eb
+</div>
 </body>
 </html>
